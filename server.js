66d7ac2e6c3e9a11346794ed2932982e8754388a
Tristan <114986392+Cybertrist@users.noreply.github.com>
2025-11-22 22:15:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 25023;
 
 // Configuration des fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
